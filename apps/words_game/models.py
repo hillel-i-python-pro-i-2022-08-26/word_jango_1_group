d288987060c7d0b87fed51557965a8accb2a7dd6
axeratos <nikitalukianitcares@gmail.com>
2022-10-21 15:10:40 +0300

    Additional functions in models

--- apps/words_game/models.py
+++ apps/words_game/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # class Word(models.Model):
@@ -8,6 +9,12 @@ from django.db import models
 class Room(models.Model):
     host = models.CharField(max_length=50)
     last_word = models.CharField(max_length=70)
+
+    def get_absolute_url(self):
+        return reverse("", kwargs={"room_id": self.pk})
+
+    def get_words(self):
+        return self.words.filter(room_id=self.pk)
 
 
 class Word(models.Model):

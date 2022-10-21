1be762101fa31e97cb67734b95327b50ecf7fe3f
axeratos <nikitalukianitcares@gmail.com>
2022-10-21 14:56:11 +0300

    Models and migrations

--- apps/words_game/models.py
+++ apps/words_game/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
 
+# class Word(models.Model):
+#     word = models.CharField(max_length=50, unique=True)
+
+
+class Room(models.Model):
+    host = models.CharField(max_length=50)
+    last_word = models.CharField(max_length=70)
+
+
 class Word(models.Model):
-    word = models.CharField(max_length=50, unique=True)
+    word = models.CharField(max_length=70)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True, related_name="words")
+
+    def __hash__(self):
+        return hash(self.word)

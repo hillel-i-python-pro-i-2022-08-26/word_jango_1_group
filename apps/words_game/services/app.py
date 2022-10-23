3e3483f7cca0598895131698f7d84c5c10fac719
axeratos <nikitalukianitcares@gmail.com>
2022-10-23 21:30:18 +0300

    Fill context for view function

--- apps/words_game/services/app.py
+++ apps/words_game/services/app.py
@@ -1,21 +1,6 @@
-# from django.utils import timezone
-from apps.words_game.models import Room
-
-#
-#
-# def validate_words(input_word):
-#     required_datetime = timezone.now()
-#     last_word = Word.objects.get(input_word, ).filter(created_at__lt=required_datetime)
-#     if input_word[-1] == last_word[0]:
-#
-#
-
-
-def validate_words(new_word, room_name):
-    room = Room.objects.get(room_name=room_name)
-    last_word = room.last_word
-    all_words = set(map(lambda obj: obj.word, room.words.all()))
-    if last_word:
-        if new_word[0] != last_word[-1] or new_word in all_words:
-            return False
-    return True
+def fill_context(room_obj):
+    return {
+        "room_name": room_obj.room_name,
+        "previous_words": room_obj.words.all(),
+        "last_word": room_obj.last_word,
+    }
